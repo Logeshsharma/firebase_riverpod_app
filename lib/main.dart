345988a3f7a01c518cc384d94b9be1d7e9cbd326
Logeshsharma <logeshsharmak@gmail.com>
2024-11-24 22:06:48 +0000

    Add firebase auth using email and password

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_firebase_app/counter_provider.dart';
+import 'package:riverpod_firebase_app/counter/counter_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyBL0B_KRSAwg0B2BoJ-7ag6yNwS-oCJSEI",
+        authDomain: "fir-riverpod-app-37497.firebaseapp.com",
+        projectId: "firebase-riverpod-app",
+        storageBucket: "firebase-riverpod-app.firebasestorage.app",
+        messagingSenderId: "462408769873",
+        appId: "1:462408769873:web:c58dbeee12716b3669f370"),
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -12,46 +23,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: const CounterView(),
-    );
-  }
-}
-
-class CounterView extends ConsumerWidget {
-  const CounterView({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final count =  ref.read(counterProvider);
-    return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Counter App'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$count',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => ref.watch(counterProvider.notifier).increment(),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
